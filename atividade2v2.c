99303c05235e273f5006796013f10c2678dda508
Tennison Capra <tennison_capra@hotmail.com>
2022-02-08 19:11:28 -0300

    add atividade2v2

---
+++ atividade2v2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Atividade 2 v
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	float F, C, K;
+	int n = 1;
+	
+	do{
+		printf("\nDigite a temperatura em graus Fahrenheit: ");
+		scanf("%f", &F);
+		C = (F-32)*5/9;
+		K = C + 273.15;
+		
+		printf("\nSegue a temperatura em Celsius: %.2f \ºC\nSegue a temperatura em Kelvin: %.2f \ºK.\n\n", C, K);
+		printf("Sensação térmica: ");
+		if(C<15){
+			printf("Frio\n");
+		}else{
+			if(C<=30){
+				printf("Normal\n");
+			}else{
+				printf("Quente\n");
+			}
+		}
+		printf("---------------------------\n");
+		n++;	
+	}while(n<=30);
+	
+	return 0;
+}
+
+
