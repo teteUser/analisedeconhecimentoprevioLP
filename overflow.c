49eeeda30ab9651b52eb2e52326ecc93d6331d42
Tennison Capra <tennison_capra@hotmail.com>
2022-02-08 18:51:31 -0300

    exercicios 1 e 2

---
+++ overflow.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Overflow
+/*
+int main(){
+    int N, P, Q;
+    char C;
+
+    scanf("%d", &N);
+    scanf("%d %c %d", &P, &C, &Q);
+
+    if(C == '+'){
+        if(P+Q <= N){
+            printf("OK");
+        }else{
+            printf("OVERFLOW");
+        }
+    }
+
+    if(C == '*'){
+       if(P*Q <= N){
+            printf("OK");
+        }else{
+            printf("OVERFLOW");
+        }
+    }
+
+    return 0;
+}
+*/
---
+++ papel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Aviões de Papel
+/*
+int main(){
+    int c, p, f;
+
+    scanf("%i %i %i", &c, &p, &f);
+
+    if(f <= (p/c)){
+        printf("S");
+    }else{
+        printf("N");
+    }
+
+    return 0;
+}
+*/
